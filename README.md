869200ca8f4659aa766bcc28f1db088b4224f04f
Jesús Fernández Jodar <jesus.fernandez@gesthispania.com>
2024-10-13 10:03:35 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Learning Resources App
+
+## Description
+
+Small Vue App for managing a learning resources plattform
+
+## Installation
+
+1. Clone the repository:
+   ```bash
+   git clone https://github.com/gsusgit/vue-learning-app.git
---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <ul>
+        
+    </ul>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                storedResources: [
+                    {
+                        id: 'official-guide',
+                        title: 'Official Guide',
+                        description: 'The official Vue JS Guide',
+                        link: 'https://www.vuejs.org'
+                    },
+                    {
+                        id: 'google',
+                        title: 'Google',
+                        description: 'Learn to google...',
+                        link: 'https://www.google.com'
+                    }
+                ]
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
